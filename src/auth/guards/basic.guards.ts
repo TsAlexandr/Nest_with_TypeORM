e6787dd715b5f46c1798164718bc99c0950007ea
TsAlexandr <lunoface@mail.ru>
2022-08-06 17:27:13 +0700

    add basic authorization with fixed pass and login

--- src/auth/guards/basic.guards.ts
+++ src/auth/guards/basic.guards.ts
@@ -1,3 +1,28 @@
-import { AuthGuard } from '@nestjs/passport';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
 
-export class BasicGuards extends AuthGuard('basic') {}
+@Injectable()
+export class BasicGuards implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const input = 'Basic YWRtaW46cXdlcnR5';
+    if (!request.headers || !request.headers.authorization) {
+      throw new BadRequestException({ message: 'Where is your header?' });
+    } else {
+      if (request.headers.authorization != input) {
+        throw new UnauthorizedException({
+          message: 'Invalid pass or login',
+        });
+      }
+      return true;
+    }
+  }
+}
