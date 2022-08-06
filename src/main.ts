a828809404d0e654587ec90cd33c9c6ebbbd36cc
TsAlexandr <lunoface@mail.ru>
2022-08-06 17:30:11 +0700

    another way to use the global pipes for extracting errors, because my pipe extract another properties instead necessary

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './pipes/validation.pipes';
 import * as cookieParser from 'cookie-parser';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationException } from './exceptions/validation.exception';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: false,
+      exceptionFactory: (errors) => {
+        const customErrors = errors.map((e) => {
+          const firstError = JSON.stringify(e.constraints);
+          return { field: e.property, message: firstError };
+        });
+        throw new BadRequestException(customErrors);
+      },
+    }),
+  );
+  app.useGlobalFilters(new ValidationException());
   app.use(cookieParser());
   await app.listen(PORT, () => console.log('server started' + PORT));
 }
