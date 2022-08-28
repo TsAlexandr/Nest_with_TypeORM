7dbe6273cb7d1e761a5d5280a74ff680d27b7a72
TsAlexandr <lunoface@mail.ru>
2022-08-28 22:21:10 +0700

    remove

--- constants.ts
+++
@@ -1,7 +0,0 @@
-export const jwtConstants = {
-  secret: process.env.JWT_SECRET || 'secret',
-};
-export const basicConstants = {
-  login: process.env.LOGIN || 'admin',
-  password: process.env.PASSWORD || '123',
-};
