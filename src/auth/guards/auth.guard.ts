aa1d69750f5a34add45a0e8dd343fd0bb0ba2911
TsAlexandr <lunoface@mail.ru>
2022-08-28 22:13:22 +0700

    remove jwt-passport, use jot

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -7,15 +7,11 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { UsersRepository } from '../../users/users.repository';
-import { JwtService } from '@nestjs/jwt';
-import { jwtConstants } from '../../../constants';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    private usersRepository: UsersRepository,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private usersRepository: UsersRepository) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
     if (!req.headers || !req.headers.authorization) {
@@ -27,7 +23,7 @@ export class AuthGuard implements CanActivate {
       throw new BadRequestException({ message: 'Invalid pass or login' });
     }
     try {
-      const decode: any = this.jwtService.verify(token, jwtConstants);
+      const decode: any = jwt.verify(token, process.env.JWT_SECRET_KEY!);
       const user = await this.usersRepository.findById(decode.userId);
       if (!user) {
         throw new NotFoundException({
