b69b9b5e4455181a3b6fc8fc67ce500e6df69e20
TsAlexandr <lunoface@mail.ru>
2022-08-28 22:17:24 +0700

    add guard to find comments owner

---
+++ src/auth/guards/commentBelongsGuard.ts
@@ -0,0 +1,24 @@
+import { CommentsService } from '../../comments/comments.service';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+@Injectable()
+export class CommentBelongsGuard implements CanActivate {
+  constructor(private commentsService: CommentsService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> | null {
+    const request: Request = context.switchToHttp().getRequest();
+    const commentId = request.params.commentId;
+    const comment = await this.commentsService.findOne(commentId, null);
+    if (!comment)
+      throw new NotFoundException({
+        message: 'comment not found',
+        field: 'commentId',
+      });
+    return true;
+  }
+}
