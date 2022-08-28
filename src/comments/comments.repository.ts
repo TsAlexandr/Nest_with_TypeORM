6687e073621924bee4a5cf9ab4b84eaaf13744e9
TsAlexandr <lunoface@mail.ru>
2022-08-28 22:19:45 +0700

    fix comments endpoints, hide _id, __v

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -1,18 +1,112 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Comment, CommentsDocument } from '../schemas/schemas.model';
+import {
+  Comment,
+  CommentsDocument,
+} from '../common/types/schemas/schemas.model';
+import { UserAccount } from '../common/types/classes/classes';
 
 export class CommentsRepository {
   constructor(
     @InjectModel(Comment.name)
     private commentsModel: Model<CommentsDocument>,
   ) {}
-  async findOne(id: string) {
-    const getCom = await this.commentsModel.findOne(
+  async findOne(id: string, userId: null) {
+    const comment = await this.commentsModel.findOne(
       { id },
-      { projection: { _id: 0, postId: 0 } },
+      {
+        _id: 0,
+        postId: 0,
+        __v: 0,
+      },
     );
-    return getCom;
+    if (!comment) return null;
+    const currentUserStatus = comment.totalActions?.find(
+      (el: { userId: string }) => el.userId === userId,
+    );
+    const likesCount = comment.totalActions?.filter(
+      (el) => el.action === 'Like',
+    ).length;
+    const dislikesCount = comment.totalActions?.filter(
+      (el) => el.action === 'Dislike',
+    ).length;
+    return {
+      addedAt: comment.addedAt,
+      content: comment.content,
+      id: comment.id,
+      userId: comment.userId,
+      userLogin: comment.userLogin,
+      likesInfo: {
+        dislikesCount: dislikesCount
+          ? dislikesCount
+          : comment.likesInfo.dislikesCount,
+        likesCount: likesCount ? likesCount : comment.likesInfo.likesCount,
+        myStatus: currentUserStatus ? currentUserStatus.action : 'None',
+      },
+    };
+  }
+
+  async getCommentWithPage(
+    postId: string,
+    page: number,
+    pageSize: number,
+    userId: string | null,
+  ) {
+    const filter = { postId };
+    const commentsForPosts = await this.commentsModel
+      .find(filter, { _id: 0, postId: 0, __v: 0 })
+      .limit(pageSize)
+      .skip((page - 1) * pageSize)
+      .lean();
+    const total = await this.commentsModel.countDocuments(filter);
+    const pages = Math.ceil(total / pageSize);
+
+    const commentAfterDeleteField = commentsForPosts.map((obj) => {
+      const currentUserStatus = obj.totalActions?.find(
+        (el: { userId: string }) => el.userId === userId,
+      );
+      const likesCount = obj.totalActions?.filter(
+        (el) => el.action === 'Like',
+      ).length;
+      const dislikesCount = obj.totalActions?.filter(
+        (el) => el.action === 'Dislike',
+      ).length;
+      return {
+        addedAt: obj.addedAt,
+        content: obj.content,
+        id: obj.id,
+        likesInfo: {
+          dislikesCount: dislikesCount,
+          likesCount: likesCount,
+          myStatus: currentUserStatus ? currentUserStatus.action : 'None',
+        },
+        userId: obj.userId,
+        userLogin: obj.userLogin,
+      };
+    });
+    return {
+      pagesCount: pages,
+      page: page,
+      pageSize: pageSize,
+      totalCount: total,
+      items: commentAfterDeleteField,
+    };
+  }
+
+  async createComment(newComment: Comment): Promise<Comment | null> {
+    await this.commentsModel.create(newComment);
+    const newComma = await this.commentsModel.findOne<Comment>(
+      { id: newComment.id },
+      {
+        postId: false,
+        _id: false,
+        __v: 0,
+      },
+    );
+    if (!newComma) {
+      return null;
+    }
+    return newComma;
   }
 
   async updateOne(id: string, content: string) {
@@ -28,39 +122,33 @@ export class CommentsRepository {
     return deleteComment.deletedCount === 1;
   }
 
-  async createComment(newComment: {
-    userLogin: string;
-    addedAt: Date;
-    id: string;
-    postId: string;
-    userId: string;
-    content: string;
-  }) {
-    return Promise.resolve(undefined);
-  }
-
-  async getCommentWithPage(
-    postId: string,
-    page: number,
-    pageSize: number,
-    userId: string,
+  async updateActions(
+    id: string,
+    status: string,
+    user: UserAccount,
+    addedAt: Date,
   ) {
-    const filter = { postId };
-    const commentsForPosts = await this.commentsModel
-      .find(filter, { projection: { _id: 0, postId: 0 } })
-      .limit(pageSize)
-      .skip((page - 1) * pageSize)
-      .lean();
-    const total = await this.commentsModel.countDocuments(filter);
-    const pages = Math.ceil(total / pageSize);
-
-    const commInPages = {
-      pagesCount: pages,
-      page: page,
-      pageSize: pageSize,
-      totalCount: total,
-      items: commentsForPosts,
-    };
-    return commInPages;
+    if (status === 'Like' || status === 'Dislike' || status === 'None') {
+      await this.commentsModel.updateOne(
+        { id },
+        { $pull: { totalActions: { userId: user.id } } },
+      );
+    }
+    if (status === 'Like' || status === 'Dislike') {
+      const updateLike = await this.commentsModel.updateOne(
+        { id },
+        {
+          $push: {
+            totalActions: {
+              addedAt,
+              action: status,
+              userId: user.id,
+              login: user.login,
+            },
+          },
+        },
+      );
+      return updateLike;
+    }
   }
 }
