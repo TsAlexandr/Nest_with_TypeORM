f4396d5ed2f765f994593615a1a198883b03c556
TsAlexandr <lunoface@mail.ru>
2022-09-04 22:16:54 +0700

    use module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,13 +1,21 @@
-// import { Module } from '@nestjs/common';
-// import { CommentsService } from './comments.service';
-// import { CommentsController } from './comments.controller';
-// import { CommentsRepository } from './comments.repository';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { Comment } from '../schemas/schemas.model';
-//
-// @Module({
-//   controllers: [CommentsController],
-//   providers: [CommentsService, CommentsRepository],
-//   imports: [MongooseModule.forFeature([{ name: 'Comments', schema: Comment }])],
-// })
-// export class CommentsModule {}
+import { forwardRef, Module } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { CommentsController } from './comments.controller';
+import { CommentsRepository } from './comments.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommentsSchema } from '../common/types/schemas/schemas.model';
+import { UsersModule } from '../users/users.module';
+import { BloggersModule } from '../bloggers/bloggers.module';
+import { PostsModule } from '../posts/posts.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Comments', schema: CommentsSchema }]),
+    forwardRef(() => UsersModule),
+    forwardRef(() => BloggersModule),
+    forwardRef(() => PostsModule),
+  ],
+  controllers: [CommentsController],
+  providers: [CommentsService, CommentsRepository],
+})
+export class CommentsModule {}
