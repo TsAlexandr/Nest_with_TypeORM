c9952a7caa0f913f13cc85d2c38361cf17a57dde
TsAlexandr <lunoface@mail.ru>
2022-09-04 22:02:32 +0700

    //

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { AuthService } from '../auth/auth.service';
+import { EmailService } from '../email/email.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema } from '../common/types/schemas/schemas.model';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(
+          'mongodb+srv://hello:rerere@cluster0.rxylv.mongodb.net/Cluster0?retryWrites=true&w=majority',
+        ),
+        MongooseModule.forFeature([{ name: 'Users', schema: UsersSchema }]),
+      ],
       controllers: [UsersController],
+      providers: [UsersService, UsersRepository, AuthService, EmailService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
