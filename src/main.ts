9f1b345fe32be1df9952dbbde036280910726d34
TsAlexandr <lunoface@mail.ru>
2022-09-20 19:48:25 +0700

    try to fix jwt token

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { ValidationException } from './common/exceptions/validation.exception';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { credentials: true },
+  });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
