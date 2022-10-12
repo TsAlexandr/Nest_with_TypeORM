1b38ad6cabe05385aa571852ed5efd5c80888544
TsAlexandr <lunoface@mail.ru>
2022-10-12 17:10:50 +0700

    fix wrong relations

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,6 +1,13 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { UserEntity } from '../../users/entities/user.entity';
 import { PostEntity } from '../../posts/entities/post.entity';
+import { TotalActionsEntity } from '../../library/entities/actions.entity';
 
 @Entity('comments')
 export class CommentEntity {
@@ -16,21 +23,10 @@ export class CommentEntity {
   userLogin: string;
   @Column('text')
   addedAt: string;
-  @Column('jsonb')
-  likesInfo: {
-    dislikesCount: number;
-    likesCount: number;
-    myStatus: string;
-  };
-  @Column('jsonb', { array: true })
-  totalActions: {
-    addedAt: string;
-    userId: string;
-    login: string;
-    action: string;
-  };
   @ManyToOne(() => PostEntity, (post) => post.comment)
   post: PostEntity;
   @ManyToOne(() => UserEntity, (user) => user.comment)
   user: UserEntity;
+  @OneToMany(() => TotalActionsEntity, (totalActions) => totalActions.postId)
+  totalActions: TotalActionsEntity[];
 }
