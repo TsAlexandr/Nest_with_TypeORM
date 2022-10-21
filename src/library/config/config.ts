e6358b7bb06c3e40bddc316dec2698df0bc256d3
TsAlexandr <lunoface@mail.ru>
2022-10-21 21:18:23 +0700

    n/e

---
+++ src/library/config/config.ts
@@ -0,0 +1,7 @@
+//TODO add this to authService or AuthController to set up cookie
+// const cookie2 = returnRefreshToken.headers['set-cookie'];
+// const cookie = loginUserAndCreatedTokens.get('Set-Cookie');
+//
+// const returnRefreshToken = await request(httpServer)
+//     .post('/auth/refresh-token')
+//     .set('Cookie', cookie);
