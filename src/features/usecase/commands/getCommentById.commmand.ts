bbe43b1602d9659f745a8e687530e9bb80468079
TsAlexandr <lunoface@mail.ru>
2022-12-13 07:35:35 +0700

    add command for cqrs

---
+++ src/features/usecase/commands/getCommentById.commmand.ts
@@ -0,0 +1,3 @@
+export class GetCommentByIdCommmand {
+  constructor(public readonly id: string) {}
+}
---
+++ src/features/usecase/commands/getPostById.command.ts
@@ -0,0 +1,3 @@
+export class GetPostByIdCommand {
+  constructor(public readonly id: string) {}
+}
