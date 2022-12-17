d1f6277a527b8edc91414ebce389f15c3eccb37e
TsAlexandr <lunoface@mail.ru>
2022-12-17 18:45:11 +0700

    fix path in for get comment method

--- src/features/blogger/blogger.controller.ts
+++ src/features/blogger/blogger.controller.ts
@@ -64,7 +64,7 @@ export class BloggerController {
     return bloggers;
   }
 
-  @Get()
+  @Get('comments')
   async getAllBloggerComments(
     @Query() query,
     @CurrentUserId() ownerId: string,
