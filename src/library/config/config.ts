fce3dea6e910f8e02b6d62f60adf50d4bfb04968
TsAlexandr <lunoface@mail.ru>
2022-12-17 17:38:15 +0700

    change todo

--- src/library/config/config.ts
+++ src/library/config/config.ts
@@ -1,4 +1,4 @@
-//TODO add this to authService or AuthController to set up cookie
+//TODO add this to tests to set up cookie
 // const cookie2 = returnRefreshToken.headers['set-cookie'];
 // const cookie = loginUserAndCreatedTokens.get('Set-Cookie');
 //
