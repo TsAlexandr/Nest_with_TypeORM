7979738a124e76a4fc299620a4deb4ce0722b265
TsAlexandr <lunoface@mail.ru>
2022-12-17 07:00:04 +0700

    add user controller for blogger

---
+++ src/features/blogger/blogger-users.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuards } from '../public/auth/guards/jwt-auth.guards';
+import { BanBlogDto } from './dto/banBlog.dto';
+import { Pagination } from '../../common/types/classes/pagination';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { GetBannedUserForBloggerCommand } from '../usecase/queryCommands/getBannedUserForBlogger.command';
+import { BanUserForBlogCommand } from '../usecase/commands/banUserForBlog.command';
+import { CurrentUserId } from '../../common/custom-decorator/current.user.decorator';
+
+@UseGuards(JwtAuthGuards)
+@Controller('/blogger/users')
+export class BloggerUsersController {
+  constructor(private queryBus: QueryBus, private commandBus: CommandBus) {}
+
+  @Get('blog/:id')
+  async getAllBannedUsers(
+    @Query() query,
+    @Param('id') id: string,
+    @CurrentUserId() ownerId: string,
+  ) {
+    const { page, pageSize, sortBy, sortDirection, searchLoginTerm } =
+      Pagination.getPaginationDataForUser(query);
+    return this.queryBus.execute(
+      new GetBannedUserForBloggerCommand(
+        page,
+        pageSize,
+        sortBy,
+        sortDirection,
+        searchLoginTerm,
+        id,
+        ownerId,
+      ),
+    );
+  }
+
+  @Put(':id/ban')
+  async banUserForBlog(
+    @Param('id') id: string,
+    @Body() banBlogDto: BanBlogDto,
+    @CurrentUserId() ownerId: string,
+  ) {
+    return this.commandBus.execute(new BanUserForBlogCommand(id, banBlogDto));
+  }
+}
