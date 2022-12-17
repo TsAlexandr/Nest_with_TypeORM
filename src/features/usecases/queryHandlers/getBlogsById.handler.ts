acc51276c849fbc610fdda6c13899a1cf18f8302
TsAlexandr <lunoface@mail.ru>
2022-12-17 21:13:02 +0700

    add 404 error

--- src/features/usecases/queryHandlers/getBlogsById.handler.ts
+++ src/features/usecases/queryHandlers/getBlogsById.handler.ts
@@ -1,12 +1,15 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GetBlogsByIdCommand } from '../queryCommands/getBlogsById.command';
 import { BlogsRepository } from '../../public/blogs/blogs.repository';
+import { NotFoundException } from '@nestjs/common';
 
 @QueryHandler(GetBlogsByIdCommand)
 export class GetBlogsByIdHandler implements IQueryHandler<GetBlogsByIdCommand> {
   constructor(private blogsRepository: BlogsRepository) {}
   async execute(query: GetBlogsByIdCommand) {
     const { id } = query;
-    return this.blogsRepository.getBlogsById(id);
+    const blog = await this.blogsRepository.getBlogsById(id);
+    if (!blog?.banInfo.isBanned === true) throw new NotFoundException();
+    return blog;
   }
 }
